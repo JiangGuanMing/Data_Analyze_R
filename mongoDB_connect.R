e421ef625b4229fce6b4623770d44e9e976a451c
Jeff_Jiang <34905968+JiangGuanMing@users.noreply.github.com>
2018-07-17 18:25:13 +0800

    Update mongoDB_connect.R
    data -> dataframe

--- mongoDB_connect.R
+++ mongoDB_connect.R
@@ -3,4 +3,4 @@ library("mongolite")
 uri <- "mongodb://Test_User:pega#1234@172.30.3.120:27017/TG2492s" 
 con <- mongo(collection = "PKS_train", db = "TG2492s", url = uri)
 print(con) # print environment
-data <- con$find()
+dataframe <- con$find()
